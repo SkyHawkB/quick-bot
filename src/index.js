9f08d7476dcd75bf5d9f1e6625f467a24c6fa723
SkyHawkB <danielmey626@gmail.com>
2018-06-17 16:29:48 -0400

    fix ReferenceErrors

--- src/index.js
+++ src/index.js
@@ -1,64 +1,7 @@
 const Discord = require('discord.js');
 const logger = require('sky-logger');
 
-module.exports.Bot = class Bot {
-  /**
-   * @param {ClientOptions} [options] - Options for the client.
-   */
-  constructor(options = {}) {
-    this.client = new Discord.Client();
-    this.commands = [];
-    this.config = null;
-  }
-
-  /**
-   * Add an event to the bot manually.
-   *
-   * @param {string} eventName - The name of the event.
-   * @param {Function} listener - The callback function.
-   */
-  on(eventName, callback) {
-    this.client.on(eventName, listener);
-  }
-
-  /**
-   * Set the client's config, to be referenced in commands.
-   *
-   * @param {Object} config - The config object.
-   */
-  setConfig(config) {
-    this.config = config;
-  }
-
-  /**
-   * Add a command to the bot's message handler.
-   *
-   * @param {BotCommand} command - The command to add.
-   */
-  addCommand(command) {
-    if(command instanceof BotCommand) {
-      this.commands.push(command);
-    } else {
-      throw new TypeError(`Expected "command" to be a Command!`);
-    }
-  }
-
-  /**
-   * Build the client and log in to Discord.
-   *
-   * @param {string} token - The client's token.
-   */
-  build(token) {
-    this.client.on('message', (message) => {
-      logger.debug('Message recieved!');
-    });
-
-    this.client.login(token);
-    logger.success(`Connected to Discord!`);
-  }
-}
-
-module.exports.Command = class Command {
+class Command {
   /**
    * @param {string} name - The command's name.
    */
@@ -91,3 +34,63 @@ module.exports.Command = class Command {
     }
   }
 }
+
+class Bot {
+  /**
+   * @param {ClientOptions} [options] - Options for the client.
+   */
+  constructor(options = {}) {
+    this.client = new Discord.Client();
+    this.commands = [];
+    this.config = null;
+  }
+
+  /**
+   * Add an event to the bot manually.
+   *
+   * @param {string} eventName - The name of the event.
+   * @param {Function} listener - The callback function.
+   */
+  on(eventName, callback) {
+    this.client.on(eventName, listener);
+  }
+
+  /**
+   * Set the client's config, to be referenced in commands.
+   *
+   * @param {Object} config - The config object.
+   */
+  setConfig(config) {
+    this.config = config;
+  }
+
+  /**
+   * Add a command to the bot's message handler.
+   *
+   * @param {Command} command - The command to add.
+   */
+  addCommand(command) {
+    if(command instanceof Command) {
+      this.commands.push(command);
+    } else {
+      throw new TypeError(`Expected "command" to be a Command!`);
+    }
+  }
+
+  /**
+   * Build the client and log in to Discord.
+   *
+   * @param {string} token - The client's token.
+   */
+  build(token) {
+    this.client.on('message', (message) => {
+      logger.debug('Message recieved!');
+    });
+
+    this.client.login(token);
+    logger.success(`Connected to Discord!`);
+  }
+}
+
+module.exports.Bot = Bot;
+module.exports.Command = Command;
