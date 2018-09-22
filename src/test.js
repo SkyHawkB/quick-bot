0f765881724cce3e8e3e6af3ecca496861b89430
SkyHawkB <danielmey626@gmail.com>
2018-09-22 18:13:52 -0400

    untrack u

--- src/test.js
+++
@@ -1,9 +0,0 @@
-const {Bot} = require('./index.js');
-
-const bot = new Bot('!', {})
-  .on('ready', () => {console.log('ready')})
-  .addCommand('ping', (client, message, config) => {
-    message.channel.send('Pong!');
-  })
-  .build()
-  .login();
